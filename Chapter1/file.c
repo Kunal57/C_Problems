1760119447f1d209d37172ea558a99e057e0c9a9
Kunal Patel <kunalpatelx92@gmail.com>
2017-04-20 13:09:48 -0500

    Implement word count program

--- Chapter1/file.c
+++ Chapter1/file.c
@@ -21,17 +21,47 @@
 // }
 
 // Line Count
+// int main(void)
+// {
+//   int c, nl;
+
+//   nl = 0;
+//   while((c = getchar()) != EOF)
+//   {
+//     if (c == '\n')
+//     {
+//       nl++;
+//     }
+//     printf("%i\n", nl);
+//   }
+// }
+
+// Word Count
+#define IN 1
+#define OUT 0
+
 int main(void)
 {
-  int c, nl;
+  int c, nl, nw, nc, state;
+  state = OUT;
+  nl = nw = nc = 0;
 
-  nl = 0;
   while((c = getchar()) != EOF)
   {
+    nc++;
     if (c == '\n')
     {
       nl++;
     }
-    printf("%i\n", nl);
+    if (c == ' ' || c == '\n' || c == '\t')
+    {
+      state = OUT;
+    }
+    else if (state == OUT)
+    {
+      state = IN;
+      nw++;
+    }
+    printf("%i %i %i\n", nl, nw, nc);
   }
 }
