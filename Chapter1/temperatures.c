4373f87d58c5761ff0a9199741f0d8de4aa2b7a3
Kunal Patel <kunalpatelx92@gmail.com>
2017-04-19 19:32:33 -0500

    Implement temperature program with a for loop

--- Chapter1/temperatures.c
+++ Chapter1/temperatures.c
@@ -4,21 +4,29 @@
    for fahr = 0, 20, ..., 300
 */
 
+// int main(void)
+// {
+//   float fahr, celsius;
+//   int lower, upper, step;
+
+//   lower = 0;    // lower limit of temperature table
+//   upper = 300;   // upper limit
+//   step = 20;    // step size
+
+//   printf("Fahrenheit-Celsius Table\n\n");
+
+//   fahr = lower;
+//   while (fahr <= upper) {
+//     celsius = (5.0/9.0) * (fahr-32.0);
+//     printf("%3.0f %6.1f\n", fahr, celsius);
+//     fahr = fahr + step;
+//   }
+// }
+
 int main(void)
 {
-  float fahr, celsius;
-  int lower, upper, step;
-
-  lower = 0;    // lower limit of temperature table
-  upper = 300;   // upper limit
-  step = 20;    // step size
-
-  printf("Fahrenheit-Celsius Table\n\n");
-
-  fahr = lower;
-  while (fahr <= upper) {
-    celsius = (5.0/9.0) * (fahr-32.0);
-    printf("%3.0f %6.1f\n", fahr, celsius);
-    fahr = fahr + step;
+  for(int fahr = 0; fahr <= 300; fahr = fahr + 20)
+  {
+    printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr-32));
   }
 }
