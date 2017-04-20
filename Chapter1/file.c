7e3c697813e05f2214915ec85b92fb946ff6066f
Kunal Patel <kunalpatelx92@gmail.com>
2017-04-20 12:41:14 -0500

    Implement character counting program

--- Chapter1/file.c
+++ Chapter1/file.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
 
+// int main(void)
+// {
+//   int c;
+
+//   while((c = getchar()) != EOF)
+//   {
+//     putchar(c);
+//   }
+// }
+
 int main(void)
 {
-  int c;
-
-  while((c = getchar()) != EOF)
+  for(int c = 0; getchar() != EOF; c++)
   {
-    putchar(c);
+    printf("%i\n", c);
   }
 }
