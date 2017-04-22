8cbca9b677af3e9fab258d26aeb1d027b5b3f73a
Kunal Patel <kunalpatelx92@gmail.com>
2017-04-22 13:51:23 -0500

    Implement 1.5.1

---
+++ Chapter1/one5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Exercise 1-6
+// EOF can be simulated by pressing Control + D
+// int main(void)
+// {
+//   int c;
+//   printf("%i\n", (c = getchar()) != EOF);
+// }
+
+// Exercise 1-7
+int main(void)
+{
+  printf("%i\n", EOF);
+}
