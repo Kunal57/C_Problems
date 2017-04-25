bf0c887543515f1bcdfb5cd8ae5ece5914103604
Kunal Patel <kunalpatelx92@gmail.com>
2017-04-25 14:02:20 -0500

    Implement Exercise 1-23

--- Chapter1/one10.c
+++ Chapter1/one10.c
@@ -1,31 +1,73 @@
 #include <stdio.h>
 
 // Exercise 1-22
+// #define MAXLINE 1000
+// #define LINELENGTH 50
+
+// int main(void)
+// {
+//   int c;
+//   int space = 0;
+//   int l = 0;
+//   int ll = 0;
+//   char line[MAXLINE];
+
+//   while ((c = getchar()) != EOF)
+//   {
+//     if (c == ' ' || ll < LINELENGTH)
+//     {
+//       space = l;
+//     }
+//     else if (ll > LINELENGTH)
+//     {
+//       ll = 0;
+//       line[space] = '\n';
+//     }
+//     line[l] = c;
+//     ll ++;
+//     l++;
+//   }
+//   printf("%s\n", line);
+// }
+
+// Exercise 1-23
 #define MAXLINE 1000
-#define LINELENGTH 50
 
 int main(void)
 {
   int c;
-  int space = 0;
-  int l = 0;
-  int ll = 0;
+  int i = 0;
+  int slash = 0;
   char line[MAXLINE];
 
   while ((c = getchar()) != EOF)
   {
-    if (c == ' ' || ll < LINELENGTH)
+    if (c == '/' && slash == 1)
     {
-      space = l;
+      i--;
+      while ((c = getchar()) != '\n')
+      {
+        i = i;
+      }
+      slash = 0;
     }
-    else if (ll > LINELENGTH)
+    else if (c == '/' && slash == 0)
     {
-      ll = 0;
-      line[space] = '\n';
+      slash = 1;
+      line[i] = c;
+      i++;
     }
-    line[l] = c;
-    ll ++;
-    l++;
+    else if (c == '\n' && line[i - 1] == '\n')
+    {
+      i = i;
+    }
+    else
+    {
+      slash = 0;
+      line[i] = c;
+      i++;
+    }
   }
-  printf("%s\n", line);
+
+  printf("\n__________String__________\n%s\n", line);
 }
