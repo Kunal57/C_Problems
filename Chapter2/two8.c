d5ef6995bbfc873460964998870e3ec630505b51
Kunal Patel <kunalpatelx92@gmail.com>
2017-04-26 13:51:52 -0500

    Implement Exercise 2-5

--- Chapter2/two8.c
+++ Chapter2/two8.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 
-void squeeze(char s1[], char s2[]);
+// Exercise 2-4
+// void squeeze(char s1[], char s2[]);
 
+// int main(void)
+// {
+
+//   char s1[6] = {'a', 'e', 'i', 'o', 'u', '\0'};
+//   int c, i;
+//   char s2[1000];
+
+//   while ((c = getchar()) != '\n')
+//   {
+//     s2[i++] = c;
+//   }
+
+//   squeeze(s1, s2);
+
+//   printf("%s\n", s1);
+
+// }
+
+// void squeeze(char s1[], char s2[])
+// {
+//   for (int i = 0; s1[i] != '\0'; i++)
+//   {
+//     for (int x = 0; s2[x] != '\0'; x++)
+//     {
+//       if (s1[i] == s2[x])
+//       {
+//         s1[i] = ' ';
+//       }
+//     }
+//   }
+// }
+
+int any(char s1[], char s2[]);
+
+// Exercise 2-5
 int main(void)
 {
 
@@ -14,22 +50,21 @@ int main(void)
     s2[i++] = c;
   }
 
-  squeeze(s1, s2);
-
-  printf("%s\n", s1);
+  printf("%i\n", any(s1, s2));
 
 }
 
-void squeeze(char s1[], char s2[])
+int any(char s1[], char s2[])
 {
-  for (int i = 0; s1[i] != '\0'; i++)
+  for (int i = 0; s2[i] != '\0'; i++)
   {
-    for (int x = 0; s2[x] != '\0'; x++)
+    for (int x = 0; s1[x] != '\0'; x++)
     {
-      if (s1[i] == s2[x])
+      if (s1[x] == s2[i])
       {
-        s1[i] = ' ';
+        return x;
       }
     }
   }
+  return -1;
 }
