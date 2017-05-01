432f1ed73df0bd60bc45e51aee90c3d4f79b0188
Kunal Patel <kunalpatelx92@gmail.com>
2017-05-01 12:48:39 -0500

    Implement Exercise 3-6

--- Chapter3/three6.c
+++ Chapter3/three6.c
@@ -2,96 +2,148 @@
 #include <math.h>
 
 // Exercise 3-5
-void itob(int n, char s[], int b);
-char hexa(int n);
+// void itob(int n, char s[], int b);
+// char hexa(int n);
+
+// int main(void)
+// {
+
+//   int b = 16;
+//   int n = 434324;
+//   char s[1000];
+
+//   itob(n, s, b);
+
+//   printf("%s\n", s);
+// }
+
+// void itob(int n, char s[], int b)
+// {
+//   int z = 0;
+
+//   for (int i = 5; i > 0; i--)
+//   {
+//     for (int k = 16; k > 0; k--)
+//     {
+//       double exp1 = k * pow(b, i);
+//       double exp2 = (k - 1) * pow(b, i);
+//       if (exp1 == 0 || exp2 == 0)
+//       {
+//         printf("%i, %i\n", i, k - 1);
+//         printf("%f, %f\n", exp1, exp2);
+//         if (z != 0)
+//         {
+//           s[z++] = hexa(0);
+//         }
+//         break;
+//       }
+//       else if (n < exp1 && n > exp2)
+//       {
+//         printf("%i, %i\n", i, k - 1);
+//         printf("%f, %f\n", exp1, exp2);
+//         s[z++] = hexa(k - 1);
+//         n = n - exp2;
+//         break;
+//       }
+//     }
+//   }
+
+//   if (n > 0)
+//   {
+//     s[z++] = hexa(n);
+//   }
+//   s[z++] = '\0';
+// }
+
+// char hexa(int n)
+// {
+//   switch (n)
+//   {
+//     case 0:
+//       return '0';
+//     case 1:
+//       return '1';
+//     case 2:
+//       return '2';
+//     case 3:
+//       return '3';
+//     case 4:
+//       return '4';
+//     case 5:
+//       return '5';
+//     case 6:
+//       return '6';
+//     case 7:
+//       return '7';
+//     case 8:
+//       return '8';
+//     case 9:
+//       return '9';
+//     case 10:
+//       return 'A';
+//     case 11:
+//       return 'B';
+//     case 12:
+//       return 'C';
+//     case 13:
+//       return 'D';
+//     case 14:
+//       return 'E';
+//     case 15:
+//       return 'F';
+//     default:
+//       return 'Z';
+//   }
+// }
+
+// Exercise 3-6
+void itoa(int number, char string[], int width);
+void reverse(int n, char string[]);
 
 int main(void)
 {
+  int number = 5678910;
+  char string[1000];
+  int width = 10;
 
-  int b = 16;
-  int n = 434324;
-  char s[1000];
+  itoa(number, string, width);
 
-  itob(n, s, b);
-
-  printf("%s\n", s);
+  printf("%s\n", string);
 }
 
-void itob(int n, char s[], int b)
+void itoa(int number, char string[], int width)
 {
+  int n = number;
   int z = 0;
 
-  for (int i = 5; i > 0; i--)
+  while (z != width)
   {
-    for (int k = 16; k > 0; k--)
+    if (number == 0)
     {
-      double exp1 = k * pow(b, i);
-      double exp2 = (k - 1) * pow(b, i);
-      if (exp1 == 0 || exp2 == 0)
-      {
-        printf("%i, %i\n", i, k - 1);
-        printf("%f, %f\n", exp1, exp2);
-        if (z != 0)
-        {
-          s[z++] = hexa(0);
-        }
-        break;
-      }
-      else if (n < exp1 && n > exp2)
-      {
-        printf("%i, %i\n", i, k - 1);
-        printf("%f, %f\n", exp1, exp2);
-        s[z++] = hexa(k - 1);
-        n = n - exp2;
-        break;
-      }
+      string[z++] = ' ';
+    }
+    else
+    {
+      n = number % 10;
+      number = number / 10;
+      string[z++] = n + '0';
     }
   }
+  string[z] = '\0';
 
-  if (n > 0)
-  {
-    s[z++] = hexa(n);
-  }
-  s[z++] = '\0';
+  reverse(z, string);
 }
 
-char hexa(int n)
+void reverse(int n, char string[])
 {
-  switch (n)
+  int temp;
+  int i, x;
+  x = n - 1;
+
+  for (i = 0; i <= x/2; i++)
   {
-    case 0:
-      return '0';
-    case 1:
-      return '1';
-    case 2:
-      return '2';
-    case 3:
-      return '3';
-    case 4:
-      return '4';
-    case 5:
-      return '5';
-    case 6:
-      return '6';
-    case 7:
-      return '7';
-    case 8:
-      return '8';
-    case 9:
-      return '9';
-    case 10:
-      return 'A';
-    case 11:
-      return 'B';
-    case 12:
-      return 'C';
-    case 13:
-      return 'D';
-    case 14:
-      return 'E';
-    case 15:
-      return 'F';
-    default:
-      return 'Z';
+    temp = string[i];
+    string[i] = string[x - i];
+    string[x - i] = temp;
   }
 }
