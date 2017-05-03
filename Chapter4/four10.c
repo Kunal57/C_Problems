a4b9e54aae72f03b617d9bddd248d574558dca86
Kunal Patel <kunalpatelx92@gmail.com>
2017-05-03 15:46:50 -0500

    Implement Exercise 4-12

--- Chapter4/four10.c
+++ Chapter4/four10.c
@@ -1,25 +1,58 @@
 #include <stdio.h>
 
 // Exercise 4-13
-void reverse(char s[], int begin, int end);
+// void reverse(char s[], int begin, int end);
+
+// int main(void)
+// {
+//   char s[] = "This is a string!";
+
+//   reverse(s, 0, 16); // Not including '\0' character at the end of the string
+
+//   printf("%s\n", s);
+// }
+
+// void reverse(char s[], int begin, int end)
+// {
+//   int temp;
+//   if (begin < end)
+//   {
+//     temp = s[begin];
+//     s[begin] = s[end];
+//     s[end] = temp;
+//     reverse(s, begin + 1, end - 1);
+//   }
+// }
+
+// Exercise 4-12
+void printd(int n);
+void itoa(int n, char s[]);
 
 int main(void)
 {
-  char s[] = "This is a string!";
+  int n = -1234;
+  char s[1000];
 
-  reverse(s, 0, 16); // Not including '\0' character at the end of the string
+  itoa(n, s);
 
   printf("%s\n", s);
 }
 
-void reverse(char s[], int begin, int end)
+void itoa(int n, char s[])
 {
-  int temp;
-  if (begin < end)
+  static int position = 0;
+
+  if (n < 0)
   {
-    temp = s[begin];
-    s[begin] = s[end];
-    s[end] = temp;
-    reverse(s, begin + 1, end - 1);
+    s[position++] = '-';
+    n = -n;
   }
+
+  if (n / 10)
+  {
+    itoa(n / 10, s);
+  }
+  n = n % 10;
+  s[position++] = n + '0';
+  s[position] = '\0';
 }
