9bb756c86e8847dcc2ed04dd3a5455cbc53ea592
Kunal Patel <kunalpatelx92@gmail.com>
2017-05-10 15:33:52 -0500

    Implement Exercise 5-4

--- Chapter5/five5.c
+++ Chapter5/five5.c
@@ -1,44 +1,100 @@
 #include <stdio.h>
 
 // Exercise 5-3
+// #define MAX 1000
+
+// void getsline(char *string);
+// void strcatt(char *s1, char *s2);
+
+// int main(void)
+// {
+//   char s1[MAX], s2[MAX];
+
+//   getsline(s1);
+//   getsline(s2);
+
+//   strcatt(s1, s2);
+
+//   printf("%s\n", s1);
+// }
+
+// void getsline(char *string)
+// {
+//   int c;
+//   while((c = getchar()) != '\n')
+//   {
+//     *string++ = c;
+//   }
+//   *string = '\0';
+// }
+
+// void strcatt(char *s1, char *s2)
+// {
+//   while(*s1++ != '\0')
+//   {
+//     if(*s1 == '\0')
+//     {
+//       while(*s2 != '\0')
+//       {
+//         *s1++ = *s2++;
+//       }
+//       *s1 = '\0';
+//     }
+//   }
+// }
+
+// Exercise 3-4
 #define MAX 1000
 
-void getsline(char *string);
-void strcatt(char *s1, char *s2);
+int strlength(char *s);
+int strendd(char *s1, char *s2);
 
 int main(void)
 {
-  char s1[MAX], s2[MAX];
+  char s1[] = "This is a string";
+  char s2[] = "string";
 
-  getsline(s1);
-  getsline(s2);
+  printf("%i, %i\n", *s1, *s2);
 
-  strcatt(s1, s2);
-
-  printf("%s\n", s1);
+  printf("%i\n", strendd(s1, s2));
 }
 
-void getsline(char *string)
+int strendd(char *s1, char *s2)
 {
-  int c;
-  while((c = getchar()) != '\n')
+  while(*s1 != '\0')
   {
-    *string++ = c;
+    s1++;
   }
-  *string = '\0';
-}
+  s1--;
 
-void strcatt(char *s1, char *s2)
-{
-  while(*s1++ != '\0')
+  while (*s2 != '\0')
   {
-    if(*s1 == '\0')
+    s2++;
+  }
+  s2--;
+
+  for(int i = 0; i < strlength(s2); i++)
+  {
+    printf("%c, %c\n", *s1, *s2);
+    if(*s1-- != *s2--)
     {
-      while(*s2 != '\0')
-      {
-        *s1++ = *s2++;
-      }
-      *s1 = '\0';
+      return 0;
     }
   }
+
+  return 1;
+}
+
+int strlength(char *s)
+{
+  int i = 0;
+  while (*s++ != '\0')
+  {
+    i++;
+    if (*s == '\0')
+    {
+      return i;
+    }
+  }
+  return 0;
 }
