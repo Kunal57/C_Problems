1a5d5e4d68343920364f627d11dd336bd0440373
Kunal Patel <kunalpatelx92@gmail.com>
2017-05-10 15:43:10 -0500

    Implement Exercise 5-5

--- Chapter5/five5.c
+++ Chapter5/five5.c
@@ -44,57 +44,80 @@
 // }
 
 // Exercise 3-4
-#define MAX 1000
+// #define MAX 1000
 
-int strlength(char *s);
-int strendd(char *s1, char *s2);
+// int strlength(char *s);
+// int strendd(char *s1, char *s2);
+
+// int main(void)
+// {
+//   char s1[] = "This is a string";
+//   char s2[] = "string";
+
+//   printf("%i, %i\n", *s1, *s2);
+
+//   printf("%i\n", strendd(s1, s2));
+// }
+
+// int strendd(char *s1, char *s2)
+// {
+//   while(*s1 != '\0')
+//   {
+//     s1++;
+//   }
+//   s1--;
+
+//   while (*s2 != '\0')
+//   {
+//     s2++;
+//   }
+//   s2--;
+
+//   for(int i = 0; i < strlength(s2); i++)
+//   {
+//     printf("%c, %c\n", *s1, *s2);
+//     if(*s1-- != *s2--)
+//     {
+//       return 0;
+//     }
+//   }
+
+//   return 1;
+// }
+
+// int strlength(char *s)
+// {
+//   int i = 0;
+//   while (*s++ != '\0')
+//   {
+//     i++;
+//     if (*s == '\0')
+//     {
+//       return i;
+//     }
+//   }
+//   return 0;
+// }
+
+// Exercise 5-5
+#define MAX 1000
+void strncpyy(char *s, char *t, int n);
 
 int main(void)
 {
   char s1[] = "This is a string";
-  char s2[] = "string";
+  char s2[MAX];
+  int n = 10;
 
-  printf("%i, %i\n", *s1, *s2);
+  strncpyy(s2, s1, n);
 
-  printf("%i\n", strendd(s1, s2));
+  printf("%s\n", s2);
 }
 
-int strendd(char *s1, char *s2)
+void strncpyy(char *s, char *t, int n)
 {
-  while(*s1 != '\0')
+  for(int i = 0; i < n; i++)
   {
-    s1++;
+    *s++ = *t++;
   }
-  s1--;
-
-  while (*s2 != '\0')
-  {
-    s2++;
-  }
-  s2--;
-
-  for(int i = 0; i < strlength(s2); i++)
-  {
-    printf("%c, %c\n", *s1, *s2);
-    if(*s1-- != *s2--)
-    {
-      return 0;
-    }
-  }
-
-  return 1;
-}
-
-int strlength(char *s)
-{
-  int i = 0;
-  while (*s++ != '\0')
-  {
-    i++;
-    if (*s == '\0')
-    {
-      return i;
-    }
-  }
-  return 0;
 }
