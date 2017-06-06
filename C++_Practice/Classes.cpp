b22704f53b3837cae480fd03f1dc3df0bf995e13
Kunal Patel <kunalpatelx92@gmail.com>
2017-06-06 16:52:15 -0500

    Implement Class Example

---
+++ C++_Practice/Classes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Dog.h"
+using namespace std;
+
+int main(){
+
+  Dog dog1;
+
+  dog1.bark();
+  dog1.jump();
+
+  return 0;
+}
---
+++ C++_Practice/Dog.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Dog.h"
+
+using namespace std;
+
+void Dog::bark(){
+  cout << "Woof Woof" << endl;
+}
+
+void Dog::jump(){
+  cout << "Jumped on the table" << endl;
+}
